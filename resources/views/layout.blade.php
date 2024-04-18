fbcd903e74d65ec2158f9784192d30f29302fec3
Hunter Steffner <huntersteffner@gmail.com>
2024-04-18 11:08:33 -0400

    Additional configuration

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hunter Steffner Web Development</title>
+
+        @vite('resources/css/app.css')
+        <!-- Styles -->
+        
+    </head>
+    <body>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+    </body>
+</html>
