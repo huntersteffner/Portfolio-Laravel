8fa1094f304f89151f51a38ea766a836f4ff5493
Hunter Steffner <huntersteffner@gmail.com>
2024-07-25 19:19:06 -0400

    Class Casing name change

--- app/View/Components/Resuable/button.php
+++ app/View/Components/Resuable/button.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class button extends Component
+class Button extends Component
 {
     public string $buttonColor = 'secondary';
     public string $buttonClasses = '';
