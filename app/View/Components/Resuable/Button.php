bd090b891bec9a661f1fae619f5dddf529d33f1d
Hunter Steffner <huntersteffner@gmail.com>
2024-07-26 17:32:56 -0400

    Tailwind Fix

--- app/View/Components/Resuable/Button.php
+++ app/View/Components/Resuable/Button.php
@@ -8,9 +8,7 @@ use Illuminate\View\Component;
 
 class Button extends Component
 {
-    public string $buttonColor = 'secondary';
     public string $buttonClasses = '';
-    private string $textColor = 'text-black';
     /**
      * Create a new component instance.
      */
@@ -22,13 +20,8 @@ class Button extends Component
         public bool $newTab
     )
     {
-        if ($isPrimary)
-        {
-            $this->buttonColor = 'primary';
-            $this->textColor = 'text-white';
-        }
 
-        $this->buttonClasses = 'inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center border-2 border-black border-solid ' . $this->textColor . ' rounded-lg bg-' . $this->buttonColor . '-700 hover:bg-' . $this->buttonColor . '-800 focus:ring-4 focus:ring-' . $this->buttonColor . '900';
+        $this->buttonClasses = 'inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center border-2 border-black border-solid rounded-lg focus:ring-4';
     }
     
     /**
--- resources/views/components/portfolio/portfolio-card.blade.php
+++ resources/views/components/portfolio/portfolio-card.blade.php
@@ -13,17 +13,6 @@
       {{$item['description']}}
     </p>
   </div>
-  {{-- <a href="{{$item['deployedSite']}}" target="_blank" title=""
-    class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-    role="button">
-    View Site
-    <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-      fill="currentColor">
-      <path fill-rule="evenodd"
-        d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-        clip-rule="evenodd" />
-    </svg>
-  </a> --}}
   <x-resuable.button
     :btnText="'View Site'"
     :btnLink="$link"
@@ -34,21 +23,10 @@
   @if($item['repoAvailable'] == true)
   <x-resuable.button
     :btnText="'View Repo'"
-    :btnLink="$link"
+    :btnLink="$repoLink"
     :isPrimary="true"
     :hasArrow="false"
     :newTab="true"
 ></x-resuable.button>
-  {{-- <a href="{{$item['repoLink']}}" target="_blank" title=""
-  class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-  role="button">
-  View Repo
-    <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-      fill="currentColor">
-      <path fill-rule="evenodd"
-        d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-        clip-rule="evenodd" />
-    </svg>
-  </a> --}}
   @endif
 </section>
